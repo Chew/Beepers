5eadd0816269c543491b0b8db88889e1f9464cc3
Chew <chew@chew.pw>
2020-09-04 19:09:50 -0500

    Actually, really, I'm not kidding, fix ban listener

--- src/main/java/com/mcprohosting/beepers/listeners/BanEvent.java
+++ src/main/java/com/mcprohosting/beepers/listeners/BanEvent.java
@@ -3,16 +3,13 @@ package com.mcprohosting.beepers.listeners;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.audit.ActionType;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.GuildBanEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
-import java.util.stream.Collectors;
 
 public class BanEvent extends ListenerAdapter {
     @Override
@@ -27,31 +24,31 @@ public class BanEvent extends ListenerAdapter {
 
         EmbedBuilder embed = new EmbedBuilder();
         embed.setTitle("Ban Detected");
-        embed.addField("User", event.getUser().getAsTag() + "\n" + event.getUser().getAsMention(), true);
+        event.getGuild().retrieveAuditLogs()
+                .type(ActionType.BAN)
+                .limit(1)
+                .queue(list -> {
+                    if (list.isEmpty()) return;
+                    AuditLogEntry entry = list.get(0);
 
-        AuditLogEntry entry = getRecentBan(event.getGuild(), event.getUser());
-        String reason = entry.getReason();
-        if(reason == null) {
-            reason = "*No reason provided*";
-        }
+                    embed.addField("User", event.getUser().getAsTag() + "\n" + event.getUser().getAsMention(), true);
 
-        if(entry.getUser() == null)
-            embed.addField("Banned by", "Unknown", true);
-        else
-            embed.addField("Banned by", entry.getUser().getAsTag() + "\n" + entry.getUser().getAsMention(), true);
+                    if(entry.getUser() == null)
+                        embed.addField("Banned by", "Unknown", true);
+                    else
+                        embed.addField("Banned by", entry.getUser().getAsTag() + "\n" + entry.getUser().getAsMention(), true);
 
-        embed.addField("Reason", reason + "\n\nAdditional details may be posted below.", false);
+                    String reason = entry.getReason();
+                    if(reason == null) {
+                        reason = "*No reason provided*";
+                    }
+                    embed.addField("Reason", reason + "\n\nAdditional details may be posted below.", false);
 
-        embed.setFooter("Banned at");
-        embed.setTimestamp(Instant.now());
 
-        channel.sendMessage(embed.build()).queue();
-    }
+                    embed.setFooter("Banned at");
+                    embed.setTimestamp(Instant.now());
 
-    public AuditLogEntry getRecentBan(Guild server, User user) {
-        return server.retrieveAuditLogs().cache(false).stream()
-                .filter(it -> it.getType() == ActionType.BAN)
-                .filter(it -> it.getTargetId().equals(user.getId()))
-                .collect(Collectors.toList()).get(0); // collects actions done by user
+                    channel.sendMessage(embed.build()).queue();
+                });
     }
 }
