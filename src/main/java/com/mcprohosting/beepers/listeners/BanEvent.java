8d2cb2ceddad3e80fff3af62c5c6d5fe9ea231f6
Chew <chew@chew.pw>
2020-09-25 12:35:16 -0500

    Formatting + edit text message

--- src/main/java/com/mcprohosting/beepers/listeners/BanEvent.java
+++ src/main/java/com/mcprohosting/beepers/listeners/BanEvent.java
@@ -3,7 +3,6 @@ package com.mcprohosting.beepers.listeners;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.audit.ActionType;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.guild.GuildBanEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -12,7 +11,6 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 public class BanEvent extends ListenerAdapter {
     @Override
@@ -20,7 +18,7 @@ public class BanEvent extends ListenerAdapter {
         LoggerFactory.getLogger(this.getClass()).info("Ban Event raised.");
 
         TextChannel channel = event.getJDA().getTextChannelById("715409019308736563");
-        if(channel == null) {
+        if (channel == null) {
             LoggerFactory.getLogger(this.getClass()).error("Could not find channel, this is not good.");
             return;
         }
@@ -28,39 +26,34 @@ public class BanEvent extends ListenerAdapter {
         EmbedBuilder embed = new EmbedBuilder();
         embed.setTitle("Ban Detected");
 
-        channel.sendMessage("Ban detected for " + event.getUser().getAsMention() + "! Getting details...").queueAfter(1, TimeUnit.SECONDS, new Consumer<Message>() {
-            @Override
-            public void accept(Message message) {
-                event.getGuild().retrieveAuditLogs()
-                        .type(ActionType.BAN)
-                        .limit(1)
-                        .queue(list -> {
-                            if (list.isEmpty()) return;
-                            AuditLogEntry entry = list.get(0);
+        channel.sendMessage("Ban detected for " + event.getUser().getAsMention() + "! Getting details...")
+            .queueAfter(1, TimeUnit.SECONDS, message ->
+                event.getGuild()
+                    .retrieveAuditLogs()
+                    .type(ActionType.BAN)
+                    .limit(1)
+                    .queue(list -> {
+                        if (list.isEmpty()) return;
+                        AuditLogEntry entry = list.get(0);
 
-                            embed.addField("User", event.getUser().getAsTag() + "\n" + event.getUser().getAsMention(), true);
+                        embed.addField("User", event.getUser().getAsTag() + "\n" + event.getUser().getAsMention(), true);
 
-                            if(entry.getUser() == null)
-                                embed.addField("Banned by", "Unknown", true);
-                            else
-                                embed.addField("Banned by", entry.getUser().getAsTag() + "\n" + entry.getUser().getAsMention(), true);
+                        if (entry.getUser() == null)
+                            embed.addField("Banned by", "Unknown", true);
+                        else
+                            embed.addField("Banned by", entry.getUser().getAsTag() + "\n" + entry.getUser().getAsMention(), true);
 
-                            String reason = entry.getReason();
-                            if(reason == null) {
-                                reason = "*No reason provided*";
-                            }
-                            embed.addField("Reason", reason + "\n\nAdditional details may be posted below.", false);
+                        String reason = entry.getReason();
+                        if (reason == null) {
+                            reason = "*No reason provided*";
+                        }
+                        embed.addField("Reason", reason + "\n\nAdditional details may be posted below.", false);
 
+                        embed.setFooter("Banned at");
+                        embed.setTimestamp(Instant.now());
 
-                            embed.setFooter("Banned at");
-                            embed.setTimestamp(Instant.now());
-
-                            message.editMessage(embed.build()).queue();
-                        });
-            }
-        });
-
-
-
+                        message.editMessage("Success! View Ban Below").embed(embed.build()).queue();
+                    })
+            );
     }
 }
