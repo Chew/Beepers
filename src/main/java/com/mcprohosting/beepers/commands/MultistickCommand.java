97c997bb58322b82780fe985c5b3fe95574fdb63
Chew <chew@chew.pw>
2020-10-09 13:57:35 -0500

    Rest in peace multistick.

--- src/main/java/com/mcprohosting/beepers/commands/MultistickCommand.java
+++ src/main/java/com/mcprohosting/beepers/commands/MultistickCommand.java
@@ -17,13 +17,7 @@ public class MultistickCommand extends Command {
     @Override
     protected void execute(CommandEvent commandEvent) {
         EmbedBuilder bob = new EmbedBuilder();
-        bob.setDescription("""
-                <:multistick_0_0:715985103138062396><:multistick_1_0:715985102743666783><:multistick_2_0:715985103116959754><:multistick_3_0:715985103183937656><:multistick_4_0:715985103313961210>
-                <:multistick_0_1:715985102685077519><:multistick_1_1:715985102722826341><:multistick_2_1:715985103234269225><:multistick_3_1:715985103037137027><:multistick_4_1:715985103125479454>
-                <:multistick_0_2:715985102798192672><:multistick_1_2:715985102731214859><:multistick_2_2:715985103137800343><:multistick_3_2:715985102785478848><:multistick_4_2:715985103024554015>
-                <:multistick_0_3:715985102668300290><:multistick_1_3:715985103020621874><:multistick_2_3:715985103049982032><:multistick_3_3:715985102877753425><:multistick_4_3:715985103058370640>
-                <:multistick_0_4:715985103083405404><:multistick_1_4:715985103011971102><:multistick_2_4:715985102617706588><:multistick_3_4:715985102953381959><:multistick_4_4:715985103058370630>"""
-        );
+        bob.setDescription("Sadly the multistick command had to be retired on October 1st, 2020. It will not be forgotten... :pensive:");
         commandEvent.reply(bob.build());
     }
 }
